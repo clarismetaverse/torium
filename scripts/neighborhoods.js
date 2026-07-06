6f9f94a364050e50e9b49adf42a58dc6f048dbba
clarismetaverse <134530419+clarismetaverse@users.noreply.github.com>
2026-07-06 13:48:22 +0200

    List priority neighborhood IDs

--- scripts/neighborhoods.js
+++ scripts/neighborhoods.js
@@ -1,3 +1,5 @@
 import { MILAN_IDEALISTA_PRIORITY_NEIGHBORHOODS } from '../lib/milan-idealista-locations.js';
 
-console.log(JSON.stringify(MILAN_IDEALISTA_PRIORITY_NEIGHBORHOODS, null, 2));
+for (const location of MILAN_IDEALISTA_PRIORITY_NEIGHBORHOODS) {
+  console.log(`${location.name}: ${location.id}`);
+}
